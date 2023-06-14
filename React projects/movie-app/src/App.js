8fec6be54f88746055f050b180de5dd80cc3571d
Bhavika <bhavikamr17@gmail.com>
2023-06-14 20:33:33 +0530

    wip

--- React projects/movie-app/src/App.js
+++ React projects/movie-app/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
 
 	const saveToLocalStorage = (items) => {
 		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
-	};
+	}; 
 
 	const addFavouriteMovie = (movie) => {
 		const newFavouriteList = [...favourites, movie];
